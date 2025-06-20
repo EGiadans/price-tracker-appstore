e33f25813802b063da65f4ec515dff03082bfaa0
Eduardo Giadáns <eduardo.giadans@outlook.com>
2025-06-20 00:06:28 -0600

    update font to funnel display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { Roboto } from "next/font/google";
+import { Funnel_Display } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "theme";
 import { PropsWithChildren } from "react";
 import { CustomAppBar } from "@/components/CustomAppBar";
 
-const roboto = Roboto({
+const funnel = Funnel_Display({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-roboto",
+  // variable: "--font-funnel-display",
 });
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" className={roboto.variable}>
+    <html lang="en" className={funnel.className}>
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
